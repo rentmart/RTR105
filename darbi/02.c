376a8daf620396eab90734959a46628373206a5a
user <renatdomkov@mail.ru>
2019-09-23 18:08:16 +0300

    20190923_18_07

---
+++ darbi/02.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+ printf("Hello World!\n"); 
+ printf("Hello World!\r");
+ printf("\tHello World!");
+ printf("Hello World!\a");
+ printf("Hello World!\'");
+ printf("Hello World!\"");
+ printf("Hello World!\?");
+ printf("Hello World!\\");
+ }
+
